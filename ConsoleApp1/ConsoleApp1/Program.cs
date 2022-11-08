188905a07f8d5399d0e104e2e430f5850ce39b18
kontigantilokesh@gmail.com <kontigantilokesh@gmail.com>
2022-11-08 13:18:47 +0530

    fixing sum issue

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -14,7 +14,7 @@ namespace ConsoleApp1
             Console.WriteLine("Enter second Number  :");
             int secondnumber = Int32.Parse(Console.ReadLine());
 
-            Console.WriteLine("Sum of two numbers is :"+(firstNumber+secondnumber-1).ToString());
+            Console.WriteLine("Sum of two numbers is :"+(firstNumber+secondnumber).ToString());
             Console.ReadKey();
         }
     }
