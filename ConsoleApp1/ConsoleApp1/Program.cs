5d0ce10d5dfcaf506107abec84f156e2f3861b0d
Surendra Kumar Uppari <surendra.uppari@ivycomptech.com>
2022-11-08 12:55:09 +0530

    single line change

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Add two nubmbers \n");
-            Console.WriteLine("Enter first Number");
+            Console.WriteLine("Enter first Number :");
         }
     }
 }
